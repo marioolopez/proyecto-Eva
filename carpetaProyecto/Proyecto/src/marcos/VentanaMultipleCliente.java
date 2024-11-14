662ecb32d019cf4b86ab69520ff051536cc9dfc3
elpirrin <marquitoss0311@gmail.com>
2024-11-14 18:42:21 +0100

    mi primer cambio en mi rama

---
+++ carpetaProyecto/Proyecto/src/marcos/VentanaMultipleCliente.java
@@ -0,0 +1,33 @@
+package marcos;
+
+import javax.swing.JTabbedPane;
+
+public class VentanaMultipleCliente extends JTabbedPane {
+    private VentanaAltaCliente ventanaAltaCliente;
+    private VentanaBajaCliente ventanaBajaCliente;
+
+    public VentanaMultipleCliente(VentanaPrincipal ventanaPrincipal) {
+        this.ventanaAltaCliente = new VentanaAltaCliente(ventanaPrincipal);
+        this.ventanaBajaCliente = new VentanaBajaCliente(ventanaPrincipal);
+        this.addTab("ALTA CLIENTE", this.ventanaAltaCliente);
+        this.addTab("BAJA CLIENTE", this.ventanaBajaCliente);
+        this.addChangeListener(new cambiarVentanaCliente(ventanaPrincipal));
+    }
+
+    public VentanaAltaCliente getVentanaAltaCliente() {
+        return this.ventanaAltaCliente;
+    }
+
+    public void setVentanaAltaCliente(VentanaAltaCliente ventanaAltaCliente) {
+        this.ventanaAltaCliente = ventanaAltaCliente;
+    }
+
+    public VentanaBajaCliente getVentanaBajaCliente() {
+        return this.ventanaBajaCliente;
+    }
+
+    public void setVentanaBajaCliente(VentanaBajaCliente ventanaBajaCliente) {
+        this.ventanaBajaCliente = ventanaBajaCliente;
+    }
+}
+
