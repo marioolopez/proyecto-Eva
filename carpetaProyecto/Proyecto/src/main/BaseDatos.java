27d55c26f40d1426f318ddca9d43c8cddfb41bbc
Jorge <jorgehbarrado@gmail.com>
2024-11-26 19:11:17 +0100

    s

--- carpetaProyecto/Proyecto/src/main/BaseDatos.java
+++ carpetaProyecto/Proyecto/src/main/BaseDatos.java
@@ -5,46 +5,39 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-public class BaseDatos {
+	public class BaseDatos {
+	
 	private Connection c;
-	
-	public BaseDatos() throws SQLException, ClassNotFoundException{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url = "jdbc:mysql://localhost:3306/gimnasio?useSSL=false&serverTimezone=Europe/Madrid";
-		c = DriverManager.getConnection(url, "root","root");
-		System.out.println("Conexion realidada!");
+	public BaseDatos(){
+		
 	}
-	
-	
-
+	public void conexionBD() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url="jdbc:mysql://localhost/gimnasio?serverTimezone=Europe/Madrid";
+		c= DriverManager.getConnection(url,"root","");		
+	}
 	public Connection getC() {
 		return c;
 	}
-
 	public void setC(Connection c) {
 		this.c = c;
 	}
-	
 	public void cerrarConex() throws SQLException {
 		c.close();
 	}
-	
 	public Statement crearStm() throws SQLException{
 		Statement stm;
 		stm = c.createStatement();
 		return stm;
 	}
-	
-	//sacar info
 	public ResultSet ejecutarSQL1(String cad) throws SQLException{
 		Statement stm = crearStm();
 		ResultSet rs = stm.executeQuery(cad);
 		return rs;
 	}
-	
-	//modificar, eliminar e insertar info
 	public void ejecutarSQL2(String cad) throws SQLException{
 		Statement stm = crearStm();
 		stm.executeUpdate(cad);
 	}
-}
+	
+	}
